e702a92e1294f912a802fafc0115349095ff73d3
Kundan12prasad <kundanprasad03@gmail.com>
2020-09-18 22:02:17 +0530

    changes by john

--- Git-STSDemo/src/main/java/com/practice/Git_STSDemo/App.java
+++ Git-STSDemo/src/main/java/com/practice/Git_STSDemo/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World! first change from Kundan prasad" );
+        System.out.println( "Changes done by john" );
     }
 }
